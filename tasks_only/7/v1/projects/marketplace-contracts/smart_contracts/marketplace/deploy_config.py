22dafa993024253a2fca8a69da18c2fe225b247b
satyasai69 <ch.satya.sai133@gmail.com>
2025-06-15 08:15:20 +0530

    Remove obsolete files and configurations from the Task-7-Complete_Application, including Algokit, GitHub workflows, and project-specific settings for both marketplace contracts and frontend. Streamline project structure by deleting unnecessary files to enhance maintainability.

--- tasks_only/7/v1/projects/marketplace-contracts/smart_contracts/marketplace/deploy_config.py
+++ tasks_only/7/v1/projects/marketplace-contracts/smart_contracts/marketplace/deploy_config.py
@@ -27,10 +27,11 @@ def deploy(
     app_client.deploy(
         on_schema_break=algokit_utils.OnSchemaBreak.AppendApp,
         on_update=algokit_utils.OnUpdate.AppendApp,
+       
     )
-    name = "world"
-    response = app_client.hello(name=name)
+    # name = "world"
+    # response = app_client.hello(name=name)
     logger.info(
         f"Called hello on {app_spec.contract.name} ({app_client.app_id}) "
-        f"with name={name}, received: {response.return_value}"
+       # f"with name={name}, received: {response.return_value}"
     )
